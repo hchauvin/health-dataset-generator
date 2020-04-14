461a9243f6dbdb03513beff4c7dc4efae8420dc5
Hadrien <hadrien@chauvin.io>
2020-04-14 17:30:43 +0200

    yapf

--- make.py
+++ make.py
@@ -21,7 +21,7 @@ def content_addressable_imgref(image_repository, image_id):
         image_repository: The repository to use in the image reference.
         image_id: The ID of the image.
     """
-    return image_repository + ':' + image_id[image_id.index(':')+1:][:40]
+    return image_repository + ':' + image_id[image_id.index(':') + 1:][:40]
 
 
 class Docker:
@@ -67,15 +67,21 @@ class Docker:
         else:
             imgref = Docker.DOCKER_IMAGE
 
-        run(["docker", "rm", "-f", imgref], check=False,
-            capture_output=True, env=self.env)
+        run(["docker", "rm", "-f", imgref],
+            check=False,
+            capture_output=True,
+            env=self.env)
 
         cwd = os.getcwd()
 
-        args = ["docker", "run",
-                "--name", Docker.CONTAINER_NAME,
-                "-v", f"{cwd}/notebooks:/opt/generator/notebooks",
-                ]
+        args = [
+            "docker",
+            "run",
+            "--name",
+            Docker.CONTAINER_NAME,
+            "-v",
+            f"{cwd}/notebooks:/opt/generator/notebooks",
+        ]
         for v in volumes:
             args += ["-v", v]
         args += [f'--{p}' for p in extra_parameters]
@@ -89,12 +95,16 @@ class Docker:
     def docker_build(self, tag=DOCKER_IMAGE, target=None):
         os.makedirs("target", exist_ok=True)
 
-        args = ["docker", "build", "-f", self.dockerfile, "-t", tag,
-                "--iidfile", "target/image_id.txt"]
+        args = [
+            "docker", "build", "-f", self.dockerfile, "-t", tag, "--iidfile",
+            "target/image_id.txt"
+        ]
         if self.with_cache_from:
             args += [
-                "--cache-from", Docker.DOCKER_IMAGE,
-                "--cache-from", self.intermediate_docker_image("third-party"),
+                "--cache-from",
+                Docker.DOCKER_IMAGE,
+                "--cache-from",
+                self.intermediate_docker_image("third-party"),
             ]
         if target:
             args += ["--target", target]
@@ -110,17 +120,21 @@ class Docker:
     def docker_push(self):
         intermediate_docker_images = ["synthea", "third-party"]
         for image in intermediate_docker_images:
-            self.docker_build(tag=self.intermediate_docker_image(image),
-                              target=image)
-            run(["docker", "push", self.intermediate_docker_image(image)],
-                check=True, env=self.env)
+            self.docker_build(
+                tag=self.intermediate_docker_image(image), target=image)
+            run(["docker", "push",
+                 self.intermediate_docker_image(image)],
+                check=True,
+                env=self.env)
 
         self.docker_build()
         run(["docker", "push", Docker.DOCKER_IMAGE], check=True, env=self.env)
 
         with open('target/image_reference.txt', 'r') as f:
             imgref = f.read().strip()
-        run(["docker", "tag", Docker.DOCKER_IMAGE, imgref], check=True, env=self.env)
+        run(["docker", "tag", Docker.DOCKER_IMAGE, imgref],
+            check=True,
+            env=self.env)
         run(["docker", "push", imgref], check=True, env=self.env)
 
     def intermediate_docker_image(self, name):
@@ -148,31 +162,42 @@ class Dev:
             "third-party")
 
         cwd = os.getcwd()
-        self.docker.docker_build(tag=third_party_docker_image,
-                                 target="third-party")
-        run(["docker", "rm", "-f", third_party_container_name], check=False,
+        self.docker.docker_build(
+            tag=third_party_docker_image, target="third-party")
+        run(["docker", "rm", "-f", third_party_container_name],
+            check=False,
             capture_output=True)
-        run(["docker", "create", "--name", third_party_container_name,
-             third_party_docker_image], check=True)
+        run([
+            "docker", "create", "--name", third_party_container_name,
+            third_party_docker_image
+        ],
+            check=True)
         if Path("third-party").exists():
             shutil.rmtree("third-party")
-        run(["docker", "cp",
-             f"{third_party_container_name}:/opt/generator/third-party",
-             f"{cwd}/third-party"], check=True)
+        run([
+            "docker", "cp",
+            f"{third_party_container_name}:/opt/generator/third-party",
+            f"{cwd}/third-party"
+        ],
+            check=True)
         run(["docker", "rm", third_party_container_name], check=True)
 
-        run(["mvn", "install:install-file",
-             f"-Dfile={cwd}/third-party/synthea-with-dependencies.jar",
-             "-DgroupId=org.mitre.synthea",
-             "-DartifactId=synthea",
-             "-Dversion=2.6.0-SNAPSHOT",
-             f"-DlocalRepositoryPath={cwd}/repo",
-             "-Dpackaging=jar"], check=True)
+        run([
+            "mvn", "install:install-file",
+            f"-Dfile={cwd}/third-party/synthea-with-dependencies.jar",
+            "-DgroupId=org.mitre.synthea", "-DartifactId=synthea",
+            "-Dversion=2.6.0-SNAPSHOT", f"-DlocalRepositoryPath={cwd}/repo",
+            "-Dpackaging=jar"
+        ],
+            check=True)
 
     def dev_package(self):
         self._ensure_third_party()
-        run(["mvn", "package", "-T 1.5C", "-Dmaven.test.skip=true",
-             "-DskipTests", "-P", "generator-target-eds-fat-jar"], check=True)
+        run([
+            "mvn", "package", "-T 1.5C", "-Dmaven.test.skip=true",
+            "-DskipTests", "-P", "generator-target-eds-fat-jar"
+        ],
+            check=True)
 
     def dev_test(self):
         self._ensure_third_party()
@@ -180,15 +205,19 @@ class Dev:
 
     def dev_format(self):
         self._ensure_third_party()
-        run(["mvn", "validate", "-T 1.5C", "-Dformat.check=false",
-             "-Dformat.skipTestSources=false",
-             "-Dformat.skipSources=false"], check=True)
+        run([
+            "mvn", "validate", "-T 1.5C", "-Dformat.check=false",
+            "-Dformat.skipTestSources=false", "-Dformat.skipSources=false"
+        ],
+            check=True)
 
     def dev_format_check(self):
         self._ensure_third_party()
-        run(["mvn", "validate", "-T 1.5C", "-Dformat.check=true",
-             "-Dformat.skipTestSources=false",
-             "-Dformat.skipSources=false"], check=True)
+        run([
+            "mvn", "validate", "-T 1.5C", "-Dformat.check=true",
+            "-Dformat.skipTestSources=false", "-Dformat.skipSources=false"
+        ],
+            check=True)
 
     def _ensure_third_party(self):
         if not Path("third-party").exists():
@@ -204,10 +233,13 @@ class Make:
         parser = argparse.ArgumentParser(
             description='Make health-dataset-generator',
             usage="make <command> [<args>]")
-        subcommands = [attr for attr in dir(self) if not attr.startswith("_") and callable(getattr(self, attr))]
-        parser.add_argument('command',
-                            help='Subcommand to run: one of ' + " ".join(
-                                subcommands))
+        subcommands = [
+            attr for attr in dir(self)
+            if not attr.startswith("_") and callable(getattr(self, attr))
+        ]
+        parser.add_argument(
+            'command',
+            help='Subcommand to run: one of ' + " ".join(subcommands))
         args = parser.parse_args(sys.argv[1:2])
         if not hasattr(self, args.command):
             print('Unrecognized command')
@@ -216,15 +248,20 @@ class Make:
         getattr(self, args.command)()
 
     def docker_run(self):
-        parser = argparse.ArgumentParser(
-            description='Run the docker image')
-        parser.add_argument('-b', '--build', dest='build', action='store_true',
-                            help='build the image beforehand')
-        parser.add_argument('-v', '--volume', action='append',
-                            help='A volume to mount')
-        parser.add_argument('--docker', action='append',
-                            help='Extra parameter to pass to "docker run". Example: ' +
-                                 '--docker=net=app_default')
+        parser = argparse.ArgumentParser(description='Run the docker image')
+        parser.add_argument(
+            '-b',
+            '--build',
+            dest='build',
+            action='store_true',
+            help='build the image beforehand')
+        parser.add_argument(
+            '-v', '--volume', action='append', help='A volume to mount')
+        parser.add_argument(
+            '--docker',
+            action='append',
+            help='Extra parameter to pass to "docker run". Example: ' +
+            '--docker=net=app_default')
         args = parser.parse_args(sys.argv[2:])
         self._docker().docker_run(
             build=args.build,
@@ -232,33 +269,29 @@ class Make:
             extra_parameters=args.docker or [])
 
     def docker_build(self):
-        parser = argparse.ArgumentParser(
-            description='Build the docker image')
+        parser = argparse.ArgumentParser(description='Build the docker image')
         args = parser.parse_args(sys.argv[2:])
         self._docker().docker_build()
 
     def docker_push(self):
-        parser = argparse.ArgumentParser(
-            description='Push the docker image')
+        parser = argparse.ArgumentParser(description='Push the docker image')
         args = parser.parse_args(sys.argv[2:])
         self._docker().docker_push()
 
     def dev_third_party(self):
         parser = argparse.ArgumentParser(
             description="Download/compile third-party dependencies and " +
-                        "put them in the 'third-party' directory")
+            "put them in the 'third-party' directory")
         args = parser.parse_args(sys.argv[2:])
         self._dev().dev_third_party()
 
     def dev_test(self):
-        parser = argparse.ArgumentParser(
-            description="Execute tests")
+        parser = argparse.ArgumentParser(description="Execute tests")
         args = parser.parse_args(sys.argv[2:])
         self._dev().dev_test()
 
     def dev_format(self):
-        parser = argparse.ArgumentParser(
-            description='Format source files')
+        parser = argparse.ArgumentParser(description='Format source files')
         args = parser.parse_args(sys.argv[2:])
         self._dev().dev_format()
 
@@ -276,13 +309,12 @@ class Make:
         with_cache_from = True
         if os.environ.get("CIRCLECI"):
             # Docker version is too old on CircleCI to support --cache-from.
-            # See https://github.com/moby/buildkit/issues/569 for the 
+            # See https://github.com/moby/buildkit/issues/569 for the
             # underlying issue.
             with_cache_from = False
 
         return Docker(
-            with_buildkit=with_buildkit,
-            with_cache_from=with_cache_from)
+            with_buildkit=with_buildkit, with_cache_from=with_cache_from)
 
     def _dev(self):
         return Dev(self._docker())
